34fa7466575c879c9561ba45167db49f4f76fda8
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-06-29 11:37:59 +0200

    solve 2022 day 2 part 2 in lua

--- 2022/day2/lua/solution.lua
+++ 2022/day2/lua/solution.lua
@@ -10,6 +10,24 @@ local handPower = {
   Z = 3,
 }
 
+local winResponse = {
+  A = "Y",
+  B = "Z",
+  C = "X"
+}
+
+local looseResponse = {
+  A = "Z",
+  B = "X",
+  C = "Y"
+}
+
+local drawResponse = {
+  A = "X",
+  B = "Y",
+  C = "Z"
+}
+
 local function calcOutcome(opponent, player)
   local outcome = {
     player = 0,
@@ -42,7 +60,26 @@ local function calcOutcome(opponent, player)
   return outcome
 end
 
-local matchOutcome = {
+local function choosePlayerHand(opponent, outcome)
+  -- loose
+  if outcome == "X" then
+    return looseResponse[opponent]
+  end
+  -- draw
+  if outcome == "Y" then
+    return drawResponse[opponent]
+  end
+  -- win
+  if outcome == "Z" then
+    return winResponse[opponent]
+  end
+end
+
+local firstMatchOutcome = {
+  player = 0,
+  opponent = 0
+}
+local secondMatchOutcome = {
   player = 0,
   opponent = 0
 }
@@ -52,18 +89,35 @@ for line in lines do
   for str in string.gmatch(line, "%S") do
     table.insert(t, str)
   end
-  local outcome = calcOutcome(t[1], t[2])
-  matchOutcome.player = matchOutcome.player + outcome.player
-  matchOutcome.opponent = matchOutcome.opponent + outcome.opponent
+
+  local firstOutcome = calcOutcome(t[1], t[2])
+  firstMatchOutcome.player = firstMatchOutcome.player + firstOutcome.player
+  firstMatchOutcome.opponent = firstMatchOutcome.opponent + firstOutcome.opponent
+
+  local secondOutcome = calcOutcome(t[1], choosePlayerHand(t[1], t[2]))
+  secondMatchOutcome.player = secondMatchOutcome.player + secondOutcome.player
+  secondMatchOutcome.opponent = secondMatchOutcome.opponent + secondOutcome.opponent
 end
 
 file:close()
 
-print("Player scored " .. matchOutcome.player .. " points and opponent " .. matchOutcome.opponent .. " points")
+print("--- Part One ---")
+print("Player scored " .. firstMatchOutcome.player .. " points and opponent " .. firstMatchOutcome.opponent .. " points")
 
-if (matchOutcome.player > matchOutcome.opponent) then
+if (firstMatchOutcome.player > firstMatchOutcome.opponent) then
   print("Player wins the game")
-elseif (matchOutcome.player == matchOutcome.opponent) then
+elseif (firstMatchOutcome.player == firstMatchOutcome.opponent) then
+  print("It's a draw")
+else
+  print("Player loses the game")
+end
+
+print("--- Part Two ---")
+print("Player scored " .. secondMatchOutcome.player .. " points and opponent " .. secondMatchOutcome.opponent .. " points")
+
+if (secondMatchOutcome.player > secondMatchOutcome.opponent) then
+  print("Player wins the game")
+elseif (secondMatchOutcome.player == secondMatchOutcome.opponent) then
   print("It's a draw")
 else
   print("Player loses the game")
