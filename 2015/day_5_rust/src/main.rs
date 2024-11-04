855ec9530581f58581ce5d1821b972ab7bae2fef
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2024-11-04 19:38:00 +0100

    part 2

--- 2015/day_5_rust/src/main.rs
+++ 2015/day_5_rust/src/main.rs
@@ -13,13 +13,18 @@ fn main() -> Result<(), Error> {
     let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
 
     let mut nice_string_count = 0;
+    let mut nice_string_v2_count = 0;
     for s in lines {
         if let StringType::Nice = check_string(&s) {
             nice_string_count += 1;
         }
+        if let StringType::Nice = check_string_v2(&s) {
+            nice_string_v2_count += 1;
+        }
     }
 
     println!("Nice string count {}", nice_string_count);
+    println!("Nice string v2 count {}", nice_string_v2_count);
 
     Ok(())
 }
--- 2015/day_5_rust/src/nice_string.rs
+++ 2015/day_5_rust/src/nice_string.rs
@@ -6,6 +6,15 @@ pub enum StringType {
     Naughty,
 }
 
+pub fn check_string_v2(s: &str) -> StringType {
+    if is_contains_pair_of_letters_without_overlap(s)
+        && is_contains_repeat_letter_with_one_between(s)
+    {
+        return StringType::Nice;
+    }
+    return StringType::Naughty;
+}
+
 pub fn check_string(s: &str) -> StringType {
     if is_contains_at_least_three_vowels(s)
         && is_contains_at_least_one_doublet(s)
@@ -77,6 +86,43 @@ fn is_not_contains_forbidden_strings(s: &str) -> bool {
     true
 }
 
+// part 2
+
+fn is_contains_pair_of_letters_without_overlap(s: &str) -> bool {
+    let mut index: usize = 0;
+    let last_index = s.len();
+
+    while index + 2 < last_index {
+        let pair = &s[index..=index + 1];
+
+        let check = &s[index + 2..last_index];
+        if check.contains(&pair) {
+            return true;
+        } else {
+            index += 1;
+        }
+    }
+    false
+}
+
+fn is_contains_repeat_letter_with_one_between(s: &str) -> bool {
+    let chars: Vec<char> = s.chars().collect();
+
+    let mut i = 0;
+    let mut a;
+    let mut b;
+
+    while i + 2 < chars.len() {
+        a = chars[i];
+        b = chars[i + 2];
+        if a == b {
+            return true;
+        }
+        i += 1;
+    }
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -116,8 +162,80 @@ mod tests {
     fn test_check_string() {
         assert!(matches!(check_string("ugknbfddgicrmopn"), StringType::Nice));
         assert!(matches!(check_string("aaa"), StringType::Nice));
-        assert!(matches!(check_string("jchzalrnumimnmhp"), StringType::Naughty));
-        assert!(matches!(check_string("haegwjzuvuyypxyu"), StringType::Naughty));
-        assert!(matches!(check_string("haegwjzuvuyypxyu"), StringType::Naughty));
+        assert!(matches!(
+            check_string("jchzalrnumimnmhp"),
+            StringType::Naughty
+        ));
+        assert!(matches!(
+            check_string("haegwjzuvuyypxyu"),
+            StringType::Naughty
+        ));
+        assert!(matches!(
+            check_string("haegwjzuvuyypxyu"),
+            StringType::Naughty
+        ));
+    }
+
+    #[test]
+    fn test_contains_pair_of_letters_without_overlap() {
+        assert_eq!(is_contains_pair_of_letters_without_overlap("xyxy"), true);
+        assert_eq!(
+            is_contains_pair_of_letters_without_overlap("aabcdefgaa"),
+            true
+        );
+        assert_eq!(is_contains_pair_of_letters_without_overlap("12345"), false);
+        assert_eq!(is_contains_pair_of_letters_without_overlap("aaa"), false);
+        assert_eq!(is_contains_pair_of_letters_without_overlap("aaaa"), true);
+        assert_eq!(is_contains_pair_of_letters_without_overlap("aafaa"), true);
+        assert_eq!(is_contains_pair_of_letters_without_overlap("eefee"), true);
+        assert_eq!(
+            is_contains_pair_of_letters_without_overlap("fwafehfiahewfihfefe"),
+            true
+        );
+    }
+
+    #[test]
+    fn test_contains_repeat_letter_with_one_between() {
+        assert_eq!(is_contains_repeat_letter_with_one_between("xyx"), true);
+        assert_eq!(
+            is_contains_repeat_letter_with_one_between("abcdefeghi"),
+            true
+        );
+        assert_eq!(is_contains_repeat_letter_with_one_between("aaa"), true);
+        assert_eq!(is_contains_repeat_letter_with_one_between("aav"), false);
+        assert_eq!(is_contains_repeat_letter_with_one_between("abcdcba"), true);
+        assert_eq!(
+            is_contains_repeat_letter_with_one_between("fwafehfiahewfihfefe"),
+            true
+        );
+    }
+
+    #[test]
+    fn test_check_string_v2() {
+        assert!(matches!(
+            check_string_v2("qjhvhtzxzqqjkmpb"),
+            StringType::Nice
+        ));
+        assert!(matches!(check_string_v2("xxyxx"), StringType::Nice));
+        assert!(matches!(
+            check_string_v2("uurcxstgmygtbstg"),
+            StringType::Naughty
+        ));
+        assert!(matches!(
+            check_string_v2("ieodomkazucvgmuy"),
+            StringType::Naughty
+        ));
+        assert!(matches!(
+            check_string_v2("faewkwfhaefafhuf"),
+            StringType::Nice
+        ));
+        assert!(matches!(
+            check_string_v2("faewksfhaefashuf"),
+            StringType::Naughty
+        ));
+        assert!(matches!(
+            check_string_v2("fwafehfiahewfihfefe"),
+            StringType::Nice
+        ));
     }
 }
