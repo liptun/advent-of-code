7789af96f0b54e575cde50551880850f8797077c
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2024-11-04 18:15:58 +0100

    2015 day 5 part 1

--- 2015/day_5_rust/src/main.rs
+++ 2015/day_5_rust/src/main.rs
@@ -1,6 +1,26 @@
-mod nice_string;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+};
 
-fn main() {
-    println!("Hello, world!");
+mod nice_string;
+use nice_string::*;
+
+fn main() -> Result<(), Error> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+
+    let mut nice_string_count = 0;
+    for s in lines {
+        if let StringType::Nice = check_string(&s) {
+            nice_string_count += 1;
+        }
+    }
+
+    println!("Nice string count {}", nice_string_count);
+
+    Ok(())
 }
 
--- 2015/day_5_rust/src/nice_string.rs
+++ 2015/day_5_rust/src/nice_string.rs
@@ -1,7 +1,19 @@
 use std::collections::HashMap;
 
-pub fn is_nice_string(s: &str) -> bool {
-    is_contains_at_least_three_vowels(s)
+#[derive(Debug)]
+pub enum StringType {
+    Nice,
+    Naughty,
+}
+
+pub fn check_string(s: &str) -> StringType {
+    if is_contains_at_least_three_vowels(s)
+        && is_contains_at_least_one_doublet(s)
+        && is_not_contains_forbidden_strings(s)
+    {
+        return StringType::Nice;
+    }
+    return StringType::Naughty;
 }
 
 fn get_vowels(s: &str) -> HashMap<char, u8> {
@@ -24,6 +36,47 @@ fn is_contains_at_least_three_vowels(s: &str) -> bool {
     get_vowels(s).values().sum::<u8>() >= 3
 }
 
+fn get_doublets(s: &str) -> HashMap<String, u8> {
+    let mut doublets: HashMap<String, u8> = HashMap::new();
+
+    let chars: Vec<char> = s.chars().collect();
+
+    let mut i: usize = 1;
+    let mut prev: char = chars[0];
+
+    while i < chars.len() {
+        if chars[i] == prev {
+            let key = prev.to_string().repeat(2);
+            let d = doublets.entry(key).or_insert(0);
+            *d += 1;
+            i += 1;
+        }
+
+        prev = if let Some(c) = chars.get(i) {
+            *c
+        } else {
+            break;
+        };
+        i += 1;
+    }
+
+    doublets
+}
+
+fn is_contains_at_least_one_doublet(s: &str) -> bool {
+    get_doublets(s).values().sum::<u8>() >= 1
+}
+
+fn is_not_contains_forbidden_strings(s: &str) -> bool {
+    let forbidden_strings: Vec<&str> = vec!["ab", "cd", "pq", "xy"];
+    for f in forbidden_strings.iter() {
+        if s.contains(f) {
+            return false;
+        }
+    }
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,13 +95,29 @@ mod tests {
         assert_eq!(is_contains_at_least_three_vowels("aeiouaeiouaeiou"), true);
     }
 
-    //#[test]
-    fn test_is_nice_string() {
-        assert_eq!(is_nice_string("ugknbfddgicrmopn"), true);
-        assert_eq!(is_nice_string("aaa"), true);
+    #[test]
+    fn test_get_doublets() {
+        assert_eq!(get_doublets("aaeecc").len(), 3);
+    }
 
-        assert_eq!(is_nice_string("jchzalrnumimnmhp"), false);
-        assert_eq!(is_nice_string("haegwjzuvuyypxyu"), false);
-        assert_eq!(is_nice_string("haegwjzuvuyypxyu"), false);
+    #[test]
+    fn test_contains_one_doublet() {
+        assert_eq!(is_contains_at_least_three_vowels("aei"), true);
+    }
+
+    #[test]
+    fn test_contains_forbidden_strings() {
+        assert_eq!(is_not_contains_forbidden_strings("abc"), false);
+        assert_eq!(is_not_contains_forbidden_strings("cdd"), false);
+        assert_eq!(is_not_contains_forbidden_strings("aad"), true);
+    }
+
+    #[test]
+    fn test_check_string() {
+        assert!(matches!(check_string("ugknbfddgicrmopn"), StringType::Nice));
+        assert!(matches!(check_string("aaa"), StringType::Nice));
+        assert!(matches!(check_string("jchzalrnumimnmhp"), StringType::Naughty));
+        assert!(matches!(check_string("haegwjzuvuyypxyu"), StringType::Naughty));
+        assert!(matches!(check_string("haegwjzuvuyypxyu"), StringType::Naughty));
     }
 }
